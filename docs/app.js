043823b97ae15c0a02742741565aae8999c49772
Reynir Aron Magnússon <reyniraron@gmail.com>
2019-06-01 18:59:47 +0000

    Simplify code, remove unused stuff

--- docs/app.js
+++ docs/app.js
@@ -37,48 +37,34 @@ function updateIngredients(base) {
 // idRef is the ID of the element
 // change is the number to change the value of the ID with
 function changeInputNumberValue(idRef, change) {
-    var valueWithChange = 0;
-    // only add/subtract
-    if (change == -1 || change == +1) {
-        valueWithChange = parseInt(getValue(idRef)) + change;
+    let updatedValue = 0;
+    // Make sure the value is being added to or subtracted from
+    if (change === -1 || change === +1) {
+        updatedValue = Number(area.value) + change;
     }
     else {
-        valueWithChange = change;
+        updatedValue = change;
     }
-    if (valueWithChange > 0) {
-    changeInputText(idRef, valueWithChange);
-    changePizzas(valueWithChange);
+    // If the updated value is valid (above 0), update info
+    if (updatedValue > 0) {
+        changeInputText(updatedValue);
+        changePizzas(updatedValue);
     }
     else {
-        changeInputText(idRef, 1);
+        changeInputText(1);
     }
     saveNumberOfPizzas(area.value);
 }
 
-function getValue(idRef) {
-    // Make sure browser supports getElementById
-    if(!document.getElementById) return;
-    // Find the input by it's id
-    var inputObj = document.getElementById(idRef);
-    return inputObj.value;
-}
-
 // TODO move to common
 // Assign the value to the input with the id passed in
-function changeInputText(idRef, val) {
-    // Make sure browser supports getElementById
-    if(!document.getElementById) return;
-    // Find the input by it's id
-    var inputObj = document.getElementById(idRef);
-    if (inputObj) {
-        // Update the value
-        inputObj.value = val;
-    }
+function changeInputText(val) {
+    area.value = val;
 }
 
-var area = document.createElement("input");
+const area = document.createElement("input");
 area.id = "number_of_pizzas";
-area.onkeyup = "changePizzas(value);";
+area.addEventListener("keyup", () => changePizzas(this.value));
 
 document.querySelector("#contentX").appendChild(area);
 
