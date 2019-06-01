bc18fc1bd631a4ee43b4a57218eb1852b7f35d91
Reynir Aron Magnússon <reyniraron@gmail.com>
2019-06-01 17:15:55 +0000

    Improve code style consistency

--- app.js
+++ app.js
@@ -13,59 +13,60 @@ function updateIngredients(base) {
 
 // idRef is the ID of the element
 // change is the number to change the value of the ID with
-function changeInputNumberValue( idRef, change ) {
+function changeInputNumberValue(idRef, change) {
     var valueWithChange = 0;
     // only add/subtract
-    if(change == -1 || change == +1 ) {
-        valueWithChange = parseInt(getValue( idRef )) + change;
+    if (change == -1 || change == +1 ) {
+        valueWithChange = parseInt(getValue(idRef)) + change;
     }
-    else
+    else {
         valueWithChange = change;
-    if( valueWithChange > 0 ) {
-    changeInputText( idRef, valueWithChange);
-    changePizzas( valueWithChange );
     }
-    else
+    if (valueWithChange > 0) {
+    changeInputText(idRef, valueWithChange);
+    changePizzas(valueWithChange);
+    }
+    else {
         changeInputText( idRef, 1);
-
+    }
     saveNumberOfPizzas(area.value);
 }
 
-function getValue( idRef ) {
+function getValue(idRef) {
     // Make sure browser supports getElementById
-    if(!document.getElementById ) return;
+    if(!document.getElementById) return;
     // Find the input by it's id
-    var inputObj = document.getElementById( idRef );
+    var inputObj = document.getElementById(idRef);
     return inputObj.value;
 }
 
 // TODO move to common
 // Assign the value to the input with the id passed in
-function changeInputText( idRef, val ) {
+function changeInputText(idRef, val) {
     // Make sure browser supports getElementById
-    if(!document.getElementById ) return;
+    if(!document.getElementById) return;
     // Find the input by it's id
-    var inputObj = document.getElementById( idRef );
-    if( inputObj ) {
+    var inputObj = document.getElementById(idRef);
+    if (inputObj) {
         // Update the value
         inputObj.value = val;
     }
 }
 
-var area = document.createElement('input');
+var area = document.createElement("input");
 area.id = "number_of_pizzas";
 area.onkeyup = "changePizzaz(value);";
 
-document.querySelector('#contentX').appendChild(area);
+document.querySelector("#contentX").appendChild(area);
 
 // place content from previous edit
 if (!area.value) {
-    area.value = window.localStorage.getItem('value');
+    area.value = window.localStorage.getItem("value");
 }
 
 // your content will be saved locally
-area.addEventListener('keyup', function () {
-    changeInputNumberValue(document.myForm.number_of_pizzas.id, area.value );
+area.addEventListener("keyup", function () {
+    changeInputNumberValue(document.myForm.number_of_pizzas.id, area.value);
 }, false);
 
 updateLog();
@@ -77,13 +78,13 @@ function updateLog() {
         document.querySelector("#log").innerHTML = 'last saved: ' + delta + 's ago';
     } 
     else {
-        area.value = '4';
+        area.value = "4";
     }
 }
 
-function saveNumberOfPizzas( value ) {
-    window.localStorage.setItem('value', value);
-    window.localStorage.setItem('timestamp', (new Date()).getTime());
+function saveNumberOfPizzas(value) {
+    window.localStorage.setItem("value", value);
+    window.localStorage.setItem("timestamp", (new Date()).getTime());
 }
 
 updateIngredients(document.myForm.number_of_pizzas.value);
