f378f2e36f929a8a5f6711cb0cef8708c7e51e9e
Reynir Aron Magnússon <reyniraron@gmail.com>
2019-06-02 11:14:56 +0000

    Refactor, rewrite and reorganize code

--- docs/app.js
+++ docs/app.js
@@ -1,3 +1,6 @@
+/* =======
+   Classes
+   ======= */
 // Each ingredient has a name, a base unit size and an HTML element
 class Ingredient {
     constructor(name, baseUnitSize, element) {
@@ -16,6 +19,9 @@ class Ingredient {
     }
 }
 
+/* ====
+   Data
+   ==== */
 // All ingredients that need to be updated
 const ingredients = [
     new Ingredient("water",      65,  document.getElementById("water")),
@@ -25,87 +31,109 @@ const ingredients = [
     new Ingredient("brownsugar", 3,   document.getElementById("brownsugar")),
     new Ingredient("flour",      100, document.getElementById("flour")),
 ];
+// Default number of pizzas if no number is saved in localStorage
+const defaultNoOfPizzas = 4;
 
-function changePizzas(numberOfPizzas) {
-    updateIngredients(numberOfPizzas);
-}
-
+/* =========
+   Functions
+   ========= */
+// Update the amount displayed for each ingredient
 function updateIngredients(base) {
     ingredients.forEach(ingredient => ingredient.updateAmount(base));
 }
 
-// idRef is the ID of the element
-// change is the number to change the value of the ID with
-function changeInputNumberValue(idRef, change) {
-    let updatedValue = 0;
-    // Make sure the value is being added to or subtracted from
+// Update the input field's text value
+function setDisplayedValue(val) {
+    inputField.value = val;
+}
+
+// Save chosen number of pizzas to localStorage
+function saveNumberOfPizzas(noOfPizzas) {
+    window.localStorage.setItem("value", noOfPizzas);
+    window.localStorage.setItem("timestamp", new Date().getTime());
+}
+
+// Change the value displayed in the input field
+function changeDisplayedValue(change) {
+    let newValue = 0;
+    // Make sure the change is valid (+-1)
     if (change === -1 || change === +1) {
-        updatedValue = Number(area.value) + change;
+        newValue = Number(inputField.value) + change;
     }
     else {
-        updatedValue = change;
+        newValue = change;
     }
-    // If the updated value is valid (above 0), update info
-    if (updatedValue > 0) {
-        changeInputText(updatedValue);
-        changePizzas(updatedValue);
+    // If the new value is valid (over 0), update info
+    if (newValue > 0) {
+        setDisplayedValue(newValue);
+        updateIngredients(newValue);
     }
+    // The minimum value is 1
     else {
-        changeInputText(1);
+        setDisplayedValue(1);
     }
-    saveNumberOfPizzas(area.value);
+    // Save changes
+    saveNumberOfPizzas(inputField.value);
 }
 
-// TODO move to common
-// Assign the value to the input with the id passed in
-function changeInputText(val) {
-    area.value = val;
-}
-
-const area = document.createElement("input");
-area.id = "number_of_pizzas";
-area.addEventListener("keyup", () => changePizzas(this.value));
-
-document.getElementById("input-container").appendChild(area);
-
-// place content from previous edit
-if (!area.value) {
-    area.value = window.localStorage.getItem("value");
-}
-
-// your content will be saved locally
-area.addEventListener("keyup", function () {
-    changeInputNumberValue(document.myForm.number_of_pizzas.id, area.value);
-}, false);
-
-updateLog();
-setInterval(updateLog, 5000); // show time every 5 seconds
+// Update log (shows when number of pizzas was last saved)
 function updateLog() {
-    var delta = 0;
-    if (window.localStorage.getItem('value')) {
-        delta = ((new Date()).getTime() - (new Date()).setTime(window.localStorage.getItem('timestamp'))) / 1000;
-        document.querySelector("#log").innerHTML = 'last saved: ' + delta + 's ago';
-    } 
+    let delta = 0;
+    // If value exists in local storage
+    if (window.localStorage.getItem("value")) {
+        // Get delta time from now to the time of the last save
+        delta = (new Date().getTime() - new Date().setTime(window.localStorage.getItem("timestamp"))) / 1000;
+        logContainer.textContent = `Last saved: ${delta} s ago`;
+    }
+    // If value doesn't exist, use default number of pizzas
     else {
-        area.value = "4";
+        inputField.value = defaultNoOfPizzas;
     }
 }
 
-function saveNumberOfPizzas(value) {
-    window.localStorage.setItem("value", value);
-    window.localStorage.setItem("timestamp", (new Date()).getTime());
-}
+/* ============
+   DOM elements
+   ============ */
+// Element to contain log
+const logContainer = document.getElementById("log");
 
+// Buttons to increment/decrement number of pizzas
 const minusBtn = document.getElementById("minus-btn");
 const plusBtn = document.getElementById("plus-btn");
 
+// Element to contain input field with number of pizzas
+const inputContainer = document.getElementById("input-container");
+
+// Input field with number of pizzas
+const inputField = document.createElement("input");
+inputField.id = "number_of_pizzas";
+inputField.addEventListener("keyup", () => updateIngredients(this.value));
+
+// Add input field to its container
+inputContainer.appendChild(inputField);
+
+// If input field has no value, get previous value from localStorage
+if (!inputField.value) {
+    inputField.value = window.localStorage.getItem("value");
+}
+
+/* ======
+   Events
+   ====== */
+// Allow buttons to increment/decrement number of pizzas
+minusBtn.addEventListener("click", () => changeDisplayedValue(-1));
+plusBtn.addEventListener("click", () => changeDisplayedValue(+1));
+// Allow typing a number into the input field
+inputField.addEventListener("keyup", () => changeDisplayedValue(inputField.value));
+// Prevent form from submitting when clicking buttons
 document.myForm.addEventListener("submit", e => e.preventDefault());
 
-minusBtn.addEventListener("click", () => {
-    changeInputNumberValue(document.myForm.number_of_pizzas.id, -1);
-});
-plusBtn.addEventListener("click", () => {
-    changeInputNumberValue(document.myForm.number_of_pizzas.id, +1);
-});
+/* =======
+   On load
+   ======= */
+// Log
+updateLog();
+setInterval(updateLog, 5000);  // Update log every 5 seconds
 
+// Initialize recipe
 updateIngredients(document.myForm.number_of_pizzas.value);
