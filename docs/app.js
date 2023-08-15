b9de0632ed1682fb588c7209a764bcd898da1be2
Reyn Alpha <reynalpha@outlook.com>
2023-08-15 18:13:45 +0000

    Simplify code, remove log date

--- docs/app.js
+++ docs/app.js
@@ -43,11 +43,6 @@ function updateIngredients(base) {
     ingredients.forEach(ingredient => ingredient.updateAmount(base));
 }
 
-// Update the input field's text value
-function setDisplayedValue(val) {
-    inputField.value = val;
-}
-
 // Save chosen number of pizzas to localStorage
 function saveNumberOfPizzas(noOfPizzas) {
     window.localStorage.setItem("value", noOfPizzas);
@@ -55,49 +50,20 @@ function saveNumberOfPizzas(noOfPizzas) {
 }
 
 // Change the value displayed in the input field
-function changeDisplayedValue(change) {
-    let newValue = 0;
-    // Make sure the change is valid (+-1)
-    if (change === -1 || change === +1) {
-        newValue = Number(inputField.value) + change;
-    }
-    else {
-        newValue = change;
+function setDisplayedValue(newValue) {
+    if (newValue <= 0) {
+        newValue = 1;
     }
     // If the new value is valid (over 0), update info
-    if (newValue > 0) {
-        setDisplayedValue(newValue);
-        updateIngredients(newValue);
-    }
-    // The minimum value is 1
-    else {
-        setDisplayedValue(1);
-    }
+    inputField.value = newValue;
+    updateIngredients(newValue);
     // Save changes
     saveNumberOfPizzas(inputField.value);
-}
-
-// Update log (shows when number of pizzas was last saved)
-function updateLog() {
-    let delta = 0;
-    // If value exists in local storage
-    if (window.localStorage.getItem("value")) {
-        // Get delta time from now to the time of the last save
-        delta = (new Date().getTime() - new Date().setTime(window.localStorage.getItem("timestamp"))) / 1000;
-        logContainer.textContent = `Last saved: ${delta} s ago`;
-    }
-    // If value doesn't exist, use default number of pizzas
-    else {
-        inputField.value = defaultNoOfPizzas;
-    }
 }
 
 /* ============
    DOM elements
    ============ */
-// Element to contain log
-const logContainer = document.getElementById("log");
-
 // Buttons to increment/decrement number of pizzas
 const minusBtn = document.getElementById("minus-btn");
 const plusBtn = document.getElementById("plus-btn");
@@ -122,19 +88,15 @@ if (!inputField.value) {
    Events
    ====== */
 // Allow buttons to increment/decrement number of pizzas
-minusBtn.addEventListener("click", () => changeDisplayedValue(-1));
-plusBtn.addEventListener("click", () => changeDisplayedValue(+1));
+minusBtn.addEventListener("click", () => setDisplayedValue(Number(inputField.value) - 1));
+plusBtn.addEventListener("click", () => setDisplayedValue(Number(inputField.value) + 1));
 // Allow typing a number into the input field
-inputField.addEventListener("keyup", () => changeDisplayedValue(inputField.value));
-// Prevent form from submitting when clicking buttons
+inputField.addEventListener("keyup", () => setDisplayedValue(inputField.value));
+// Prevent form from submitting if the user presses return
 document.myForm.addEventListener("submit", e => e.preventDefault());
 
 /* =======
    On load
    ======= */
-// Log
-updateLog();
-setInterval(updateLog, 5000);  // Update log every 5 seconds
-
 // Initialize recipe
 updateIngredients(document.myForm.number_of_pizzas.value);
